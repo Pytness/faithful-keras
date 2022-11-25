95918c6df434cfe891f9fd8b78ec229cbe4a1f8d
Fernando Vega <pytness@gmail.com>
2022-11-25 22:07:48 +0100

    Add missing changes

--- extract_images.py
+++ extract_images.py
@@ -44,11 +44,75 @@ os.mkdir(ext_source_img_path)
 os.mkdir(ext_target_img_path)
 
 
+
+def transform_alpha_image(img: Image):
+	# Set all transparent pixels to black
+
+	# This is done to make the model more robust to transparent blocks
+	# Get the alpha band
+	red, green, blue, alpha = img.split()
+	
+	for i, pixel in enumerate(alpha.getdata()):
+		x = i % img.width
+		y = i // img.width
+		if pixel == 0:
+			red.putpixel((x, y), 0)
+			green.putpixel((x, y), 0)
+			blue.putpixel((x, y), 0)
+
+def generate_deformations(img: Image):
+	"""Generate all 8 possible deformations of an image"""
+
+	# 1. Rotate 90 degrees
+	# 2. Rotate 180 degrees
+	# 3. Rotate 270 degrees
+	# 4. Flip horizontally
+	# 5. Flip vertically
+	# 6. Flip horizontally and rotate 90 degrees
+	# 7. Flip horizontally and rotate 270 degrees
+	# 8. Flip vertically and rotate 90 degrees
+	# 9. Flip vertically and rotate 270 degrees
+
+	# 1. Rotate 90 degrees
+	yield img.rotate(90)
+
+	# 2. Rotate 180 degrees
+	yield img.rotate(180)
+
+	# 3. Rotate 270 degrees
+	yield img.rotate(270)
+
+	# 4. Flip horizontally
+	yield img.transpose(Image.FLIP_LEFT_RIGHT)
+
+	# 5. Flip vertically
+	yield img.transpose(Image.FLIP_TOP_BOTTOM)
+
+	# 6. Flip horizontally and rotate 90 degrees
+	yield img.transpose(Image.FLIP_LEFT_RIGHT).rotate(90)
+
+	# 7. Flip horizontally and rotate 270 degrees
+	yield img.transpose(Image.FLIP_LEFT_RIGHT).rotate(270)
+
+	# 8. Flip vertically and rotate 90 degrees
+	yield img.transpose(Image.FLIP_TOP_BOTTOM).rotate(90)
+
+	# 9. Flip vertically and rotate 270 degrees
+	yield img.transpose(Image.FLIP_TOP_BOTTOM).rotate(270)
+
+	yield img
+
+
 print(f"copying {dir}")
 for filename in shared_filenames:
 
-	source_img = Image.open(f'{raw_source_img_path}/{filename}')
-	target_img = Image.open(f'{raw_target_img_path}/{filename}')
+	source_img = Image.open(f'{raw_source_img_path}/{filename}').convert('RGBA')
+	target_img = Image.open(f'{raw_target_img_path}/{filename}').convert('RGBA')
+
+	print(filename, source_img.mode)
+	transform_alpha_image(source_img)
+	print(filename, target_img.mode)
+	transform_alpha_image(target_img)
 
 	# img2 = Image.open(f'{EXTRACT_PATH}/{filename}')
 
@@ -57,5 +121,13 @@ for filename in shared_filenames:
 
 	new_filename = filename.replace('/', '.')
 
-	shutil.copy(f'{raw_source_img_path}/{filename}', f'{ext_source_img_path}/{new_filename}')
-	shutil.copy(f'{raw_target_img_path}/{filename}', f'{ext_target_img_path}/{new_filename}')
+	for i, img in enumerate(generate_deformations(source_img)):
+		img.save(f'{ext_source_img_path}/{i}_{new_filename}.png')
+
+	for i, img in enumerate(generate_deformations(target_img)):
+		img.save(f'{ext_target_img_path}/{i}_{new_filename}.png')
+
+
+
+	# shutil.copy(f'{raw_source_img_path}/{filename}', f'{ext_source_img_path}/{new_filename}')
+	# shutil.copy(f'{raw_target_img_path}/{filename}', f'{ext_target_img_path}/{new_filename}')
--- predictor.py
+++ predictor.py
@@ -10,21 +10,28 @@ encoderModel.load_weights('weights/encoder.h5')
 
 encoded_images = encoderModel.predict(test_data_x)
 
-for i in range(10):
-	fig, (ax1, ax2, ax3) = plt.subplots(3)
 
-	ax1.imshow(test_data_x[i])
-	ax2.imshow(test_data_y[i])
-	ax3.imshow(encoded_images[i])
+# plot 10 collumns of 3 images each
+columns = 10
+rows = 3
 
-	plt.savefig('output/result_prec_%s_%s.png' % (i, time.time()))
+fig, axs = plt.subplots(rows, columns, figsize=(20, 4))
+
+for i in range(columns):
+
+	axs[0, i].imshow(test_data_x[i])
+	axs[1, i].imshow(test_data_y[i])
+	axs[2, i].imshow(encoded_images[i])
+plt.savefig('output/result_test_%s_%s.png' % (i, time.time()))
 
 encoded_images = encoderModel.predict(training_data_x)
-for i in range(10):
-	fig, (ax1, ax2, ax3) = plt.subplots(3)
 
-	ax1.imshow(training_data_x[i])
-	ax2.imshow(training_data_y[i])
-	ax3.imshow(encoded_images[i])
+fig, axs = plt.subplots(rows, columns, figsize=(20, 4))
 
-	plt.savefig('output/result_train_%s_%s.png' % (i, time.time()))
+for i in range(columns):
+
+	axs[0, i].imshow(training_data_x[i])
+	axs[1, i].imshow(training_data_y[i])
+	axs[2, i].imshow(encoded_images[i])
+plt.savefig('output/result_train_%s_%s.png' % (i, time.time()))
+
